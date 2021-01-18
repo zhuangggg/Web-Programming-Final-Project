ff3fbff8feb22be5b4b3ee2202507b7a1ffe89a8
junshiuanhsieh <shiuanhsieh@gmail.com>
2021-01-19 01:56:54 +0800

    update

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,30 @@ function Home(props){
     const location = useLocation();
     const name = location.state.username
     const data = location.state.data
+    const id = location.state.userid
     console.log(data);
     return (
         <>
             <div>Hello, {name}</div>
             {data.user.projects.length!==0? data.user.projects.map(((project, index)=>
+              <div style={{flexDirection: "column"}}>
+                <div>
                 <NavLink to={{
                     pathname:`/gantt/${project.id}`,
                     state: {projectname: project.name}
                 }}>{project.name}</NavLink>
+                </div>
+                <div>
+                <NavLink to={{
+                    pathname:`/calendar/`,
+                    state: {
+                        username: name,
+                        userid: id
+                    }
+                }}>Calendar</NavLink>
+                </div>
+              </div>
+
             )):<p>no projects</p>
             }
         </>
